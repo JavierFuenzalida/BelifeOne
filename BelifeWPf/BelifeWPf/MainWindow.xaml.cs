0ebde06f276d5d705f4c72198486fe1a5fb89c4c
Javier Fuenzalida <javi.fuenzalida@alumnos.duoc.cl>
2020-05-16 16:57:03 -0400

    Inicio Proyecto

---
+++ BelifeWPf/BelifeWPf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+
+namespace BelifeWPf
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_despliegaFly_Click(object sender, RoutedEventArgs e)
+        {
+            fly.IsOpen = true;
+        }
+    }
+}
